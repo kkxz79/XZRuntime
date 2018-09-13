e1592923264f60de677445b45573c300da177b37
kkxz79 <kkxz79@126.com>
2018-09-13 17:11:21 +0800

    Initial Commit

---
+++ XZRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZRuntime
+//
+//  Created by kkxz on 2018/9/13.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZRuntime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZRuntime
+//
+//  Created by kkxz on 2018/9/13.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
